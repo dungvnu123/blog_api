a8dec88980537ace798dd58436a1fccaca7f455f
Anh Dũng Trần <trananhdung10062004@gmail.com>
2026-04-08 23:36:34 +0700

    Fix posts module import for Render deployment

--- blogapi/blogapi/asgi.py
+++ blogapi/blogapi/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogapi.blogapi.settings')
 
 application = get_asgi_application()
